36b5962da2f7f7e0acce04f47aba79c3a380dfb9
Amitav Roy <reachme@amitavroy.com>
2022-09-18 10:48:38 +0000

    Front end code implementation
    Added Login form, Login functionality, Protected route redirection logic
    based on the cookie that is being saved during login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const guestRoutes = ["/user/login"];
+    if (!guestRoutes.includes(router.pathname)) {
+      const token = Cookies.get("token");
+      (!token || token == "") && router.push("/user/login");
+    }
+  }, [router]);
   return <Component {...pageProps} />;
 }
 
