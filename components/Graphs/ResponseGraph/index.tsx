cf47bb4acb9064b8d6b42932882e2cfb8874c34e
Amitav Roy <reachme@amitavroy.com>
2022-11-24 18:28:39 +0530

    Changed the graph UI

--- components/Graphs/ResponseGraph/index.tsx
+++ components/Graphs/ResponseGraph/index.tsx
@@ -14,8 +14,6 @@ interface Props {
   graphData: Array<ISuccessResp>;
 }
 
-const data: Array<ISuccessResp> = [];
-
 const ResponseGraph: React.FC<Props> = ({ graphData }) => {
   return (
     <LineChart
@@ -24,11 +22,11 @@ const ResponseGraph: React.FC<Props> = ({ graphData }) => {
       data={graphData}
       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
     >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="" />
+      <CartesianGrid strokeDasharray="4 4" />
+      <XAxis />
       <YAxis />
       <Tooltip />
-      <Legend />
+      <Legend verticalAlign="top" />
       <Line type="monotone" dataKey="response_time" stroke="#8884d8" />
     </LineChart>
   );
